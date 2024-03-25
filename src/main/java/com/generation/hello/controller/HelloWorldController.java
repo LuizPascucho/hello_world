97b21cfff43a482e584a4ca01b89076a8a386f95
Luiz Pascucho <luizpascucho@bol.com.br>
2024-03-25 12:33:33 -0300

    first commit

---
+++ src/main/java/com/generation/hello/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.generation.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/helloworld")
+public class HelloWorldController {
+
+	@GetMapping() // Requisições do tipo GET/Busca
+	public String hello() {
+		return "Hello World <BR> As BSMs da Generation Brazil são: <BR> Orietação ao Futuro / Responsabilidade Pessoal / Mentalidade de Crescimento / Persitência / Trabalho em Equipe / Orientação ao Detalhe / Proatividade / Comunicação";
+	}
+	
+}
